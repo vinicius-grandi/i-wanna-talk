b9c9592c9e7083c2af78a9c2248cd24236ea5498
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-05-12 17:31:26 -0300

    setting the project up

---
+++ packages/client/src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.main``;
+
+function Home() {
+  return <Container>Home</Container>;
+}
+
+export default Home;
